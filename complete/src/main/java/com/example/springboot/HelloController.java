0bd0521af5579510c24a53f1cdb974f05fac8341
Bakros <sebastian.hinojosa.h@gmail.com>
2023-12-03 00:53:08 -0300

    comment

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/hola")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! Seba";
 	}
 
 }
